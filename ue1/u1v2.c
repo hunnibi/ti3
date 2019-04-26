b2baecc45cb420424890892d882fe6b215809156
evghenii.orenciuc <evghenii.orenciuc@bitingbit.com>
2019-04-26 19:24:45 +0200

    v2

---
+++ ue1/u1v2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    FILE *fp = fopen("test.dat", "r");
+
+    if (fp == NULL)
+        return 1;
+
+    int fileSum = 0;
+    int calculatedSum = 0;
+    int val1 = 0, val2 = 0;
+
+    fscanf(fp, "%d", &fileSum);
+
+    while((fscanf(fp, "%d*%d", &val1, &val2)) != EOF) {
+        calculatedSum += val1 * val2;
+    }
+
+    fclose(fp);
+
+    int result = (fileSum == calculatedSum) ? 0 : 1;
+    printf("Result: %d\n", result);
+
+    return result;
+}
