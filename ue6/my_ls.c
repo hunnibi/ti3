edae998820d20f29809beb196bc313792cdd265e
evghenii.orenciuc <evghenii.orenciuc@bitingbit.com>
2019-06-04 10:15:03 +0200

    -

--- ue6/my_ls.c
+++ ue6/my_ls.c
@@ -15,7 +15,6 @@ int scandir(const char *dirp, struct dirent ***namelist, int (*filter)(const str
 
 int list_files(char *path, int show_sorted, int show_hidden);
 int print_err_and_return_err_code(char *err);
-char* get_last_modified(char *path);
 void print_file_description(char *path_with_name, char* file_name, int show_date_modified);
 
 // warning: multiple paths are not supported, like in native 'ls'
@@ -40,35 +39,28 @@ int is_dir_entry_not_hidden(const struct dirent *entry)
    return entry->d_name[0] != '.';
 }
 
-// char* get_last_modified(char *path)
-// {   char *buffer = (char*)malloc(50 * sizeof(char));
-//
-//
-//     strftime(buffer, 50, "%d.%m.%Y %H:%M:%S", localtime(&attr.st_mtime));
-//
-//     // return ;
-//     return buffer;
-// }
-
 char* concat_string(const char *s1, const char *s2)
 {
     char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
     strcpy(result, s1);
     strcat(result, s2);
     return result;
 }
 
-// path_with_name and name parameters are used only for not to concatenate
-// or split dir path and file name
+// path_with_name and name parameters are used
+// only for not to concatenate or split dir path and file name
 void print_file_description(char *path_with_name, char* file_name, int show_date_modified)
 {
+    // warning: file name longer than 30 characters - bad output
     fprintf(stdout, "%-30s", file_name);
-    if (show_date_modified) {
+    if (show_date_modified)
+    {
         struct stat attr;
         stat(path_with_name, &attr);
         fprintf(stdout, "%s", ctime(&attr.st_mtime)); // warning: f*cking 'ctime' generates string with '\n' at the end
-    } else {
+    }
+    else
+    {
         printf("\n");
     }
 }
@@ -81,10 +73,14 @@ int list_files(char *path, int show_sorted, int show_hidden)
     struct dirent **entries;
     int number_of_entries = scandir(path, &entries, filter, sort);
 
-    if (number_of_entries < 0) {
+    if (number_of_entries < 0)
+    {
         return errno;
-    } else {
-        for (int i = 0; i < number_of_entries; i++) {
+    }
+    else
+    {
+        for (int i = 0; i < number_of_entries; i++)
+        {
             char *path_with_name = concat_string(path, entries[i]->d_name);
 
             print_file_description(path_with_name, entries[i]->d_name, show_sorted);
@@ -103,29 +99,6 @@ int print_err_and_return_err_code(char *err)
     return 1;
 }
 
-
-// void sort() {
-// // todo: implement
-// void qsort(void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *));
-// int strcmp(const char *s1, const char *s2);
-//
-// }
-
-// int list_files_in_trash(char *path, char show_sorted, char show_hidden) {
-//     DIR *dir_fd = opendir(TRASHCAN_DIRECTORY_NAME);
-//     if (!dir_fd) return print_err_and_return_err_code(strerror(errno));
-//
-//     struct dirent *dir;
-//
-//     while ((dir = readdir(dir_fd)) != NULL) {
-//         fprintf(stdout, "%s\n", dir->d_name);
-//     }
-//
-//     closedir(dir_fd);
-//
-//     return 0;
-// }
-
 /*
 ls
 ls -l : alphabet sort + last modified
@@ -134,8 +107,7 @@ ls path/to/dir or path/to/file
 
 ls -la or ls -al should be possible
 
-
 return 0 : success
 return 1 : minor problems
-
+return 2 : major problems
 */
