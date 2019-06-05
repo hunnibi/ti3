d9223b2c65da13b2556850afd7b86f7d449c399f
evghenii.orenciuc <evghenii.orenciuc@bitingbit.com>
2019-06-05 20:54:03 +0200

    working. not everything checked

--- ue6/my_ls.c
+++ ue6/my_ls.c
@@ -13,24 +13,104 @@
 int alphasort(const struct dirent **a, const struct dirent **b);
 int scandir(const char *dirp, struct dirent ***namelist, int (*filter)(const struct dirent *), int (*compar)(const struct dirent **, const struct dirent **));
 
+char* concat_string(const char *s1, const char *s2);
 int list_files(char *path, int show_sorted, int show_hidden);
 int print_err_and_return_err_code(char *err);
-void print_file_description(char *path_with_name, char* file_name, int show_date_modified);
+void print_file_description(char *path, int show_date_modified);
+int is_regular_file(const char *path);
+int exists(const char *fname);
 
 // warning: multiple paths are not supported, like in native 'ls'
-// warning: sorting or/and filter parameters MUST go first, otherwise error
+// warning: sorting or/and filter parameters MUST go first, otherwise ignored
+// warning: ls mode length must be like one of next examples: -l / -a / -la / -al
+//          otherwise it will be ignored, or it leads to error
+// warning: path longer than 50 characters - bad output
+// warning: function prints full file paths (not only file name)
 int main(int argc, char **argv)
 {
-    // TODO: add '/' sign to each path
+    char *path = ".";
+    int sorted = 0;
+    int show_hidden = 0;
 
+    if (argc == 1)
+    {
+        return list_files(path, sorted, show_hidden);
+    }
 
-    print_file_description(".fufu", ".fufu", 1);
-    // int result = list_files("./", 0, 1);
-    // if (result) {
-    //     return print_err_and_return_err_code(strerror(errno));
-    // } else {
-    //     return 0;
-    // }
+    if (argv[1][0] == '-')
+    {
+        char *ls_mode = argv[1];
+        if (strlen(ls_mode) > 3 || strlen(ls_mode) < 2) {
+            return print_err_and_return_err_code("Error: Invalid ls mode!\n");
+        }
+
+        sorted = (ls_mode[1] == 'l' || ls_mode[2] == 'l');
+        show_hidden = (ls_mode[1] == 'a' || ls_mode[2] == 'a');
+
+        if (argc > 2)
+        {
+            path = argv[2];
+        }
+    }
+    else
+    {
+        path = argv[1];
+    }
+
+    if (!exists(path))
+    {
+        printf("ls: cannot access '%s': No such file or directory\n", path);
+        return 1;
+    }
+
+    if (is_regular_file(path))
+    {
+        print_file_description(path, sorted);
+        return 0;
+    }
+    else
+    {
+        // if dir path already has '/' at the end, another '/' will not harm
+        // it will even work with multiple '/' in a row, like: path/to////file
+        /* otherwise could be checked with next code:
+            if (path[strlen(path)-1] == '/') {
+            }
+
+            but path could have format: path/to/folder///////
+            and it will be also legitimate and will work...
+            so it doesn't make sense to check or to fix it
+        */
+        char *full_path = concat_string(path, "/");
+        int result = list_files(full_path, sorted, show_hidden);
+
+        free(full_path);
+
+        if (result)
+        {
+            return print_err_and_return_err_code(strerror(errno));
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+int is_regular_file(const char *path)
+{
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+}
+
+int exists(const char *fname)
+{
+    FILE *file;
+    if ((file = fopen(fname, "r")))
+    {
+        fclose(file);
+        return 1;
+    }
     return 0;
 }
 
@@ -47,16 +127,14 @@ char* concat_string(const char *s1, const char *s2)
     return result;
 }
 
-// path_with_name and name parameters are used
-// only for not to concatenate or split dir path and file name
-void print_file_description(char *path_with_name, char* file_name, int show_date_modified)
+void print_file_description(char *path, int show_date_modified)
 {
-    // warning: file name longer than 30 characters - bad output
-    fprintf(stdout, "%-30s", file_name);
+    // warning: path longer than 50 characters - bad output
+    fprintf(stdout, "%-50s", path);
     if (show_date_modified)
     {
         struct stat attr;
-        stat(path_with_name, &attr);
+        stat(path, &attr);
         fprintf(stdout, "%s", ctime(&attr.st_mtime)); // warning: f*cking 'ctime' generates string with '\n' at the end
     }
     else
@@ -65,6 +143,7 @@ void print_file_description(char *path_with_name, char* file_name, int show_date
     }
 }
 
+// warning: function prints full file paths (not only file name)
 int list_files(char *path, int show_sorted, int show_hidden)
 {
     int (*sort)(const struct dirent **, const struct dirent **) = show_sorted ? &alphasort : 0;
@@ -77,19 +156,17 @@ int list_files(char *path, int show_sorted, int show_hidden)
     {
         return errno;
     }
-    else
+
+    for (int i = 0; i < number_of_entries; i++)
     {
-        for (int i = 0; i < number_of_entries; i++)
-        {
-            char *path_with_name = concat_string(path, entries[i]->d_name);
+        char *path_with_name = concat_string(path, entries[i]->d_name);
 
-            print_file_description(path_with_name, entries[i]->d_name, show_sorted);
+        print_file_description(path_with_name, show_sorted);
 
-            free(path_with_name);
-            free(entries[i]);
-        }
-        free(entries);
+        free(path_with_name);
+        free(entries[i]);
     }
+    free(entries);
     return 0;
 }
 
